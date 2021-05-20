8803254dc4438f05103dc68b94530e9fb0f65c82
dyna2002chhan <dyna.chhan@student.passerellesnumeriques.org>
2021-05-20 11:57:02 +0700

    from dyna

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const app = express();
+// //GET method..................................................
+// app.listen(process.env.PORT || 5000, () => console.log('Server is running......'));;
+
+// app.use(express.json());
+// app.use(express.urlencoded());
+
+// app.get('/', (req,res) => res.send('Welcome to GET POST PUT DELETE'));
+
+// let users = [
+//     {id: 1, username: 'rady', password: '123'},
+//     {id: 2, username: 'dyna', password: '456'},
+//     {id: 3, username: 'lanh', password: '789'}
+// ]
+// app.get('', (req,res)=> res.send(users));
