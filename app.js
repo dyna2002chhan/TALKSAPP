7b9ec6357032928942d8640553513e8907aa0531
dyna2002chhan <dyna.chhan@student.passerellesnumeriques.org>
2021-05-21 22:04:11 +0700

    recieve and send today

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require('express');
-const app = express();
+let app = express();
+
+app.listen(process.env.SERVER_PORT || 3000, (req,res)=>{
+    console.log("Server is running....")
+})
+//to read data json from body..........................//
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+
+app.post('/recieve', (req,res)=> {
+    
+})
