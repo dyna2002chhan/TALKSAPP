5b435d3311ccbaf1f241fe0364a1d0cf6e31ea84
dyna2002chhan <dyna.chhan@student.passerellesnumeriques.org>
2021-05-30 22:31:35 +0700

    last commit of TALKSAPP

--- public/form_login/js/form_login.js
+++ public/form_login/js/form_login.js
@@ -1,4 +1,8 @@
-let url = "http://localhost:3000/user";
+
+//Authentification.............................................................................//
+let url = "http://192.168.56.1:3000/user";
+
+
 axios.get(url).then((response) => {
     let dataUser = response.data
     let user_name = document.querySelector('.user_name');
@@ -11,6 +15,7 @@ axios.get(url).then((response) => {
         for (data of dataUser) {
             if (data.name === user_name.value && data.password === user_password.value) {
                 localStorage.setItem('username', JSON.stringify(data.name));
+                //link to another page
                 window.location.pathname = "../form_message/message.html";
             }
 
--- public/form_message/js/form_message.js
+++ public/form_message/js/form_message.js
@@ -1,67 +1,161 @@
-const url = "http://localhost:3000/addmessageuser";
+const url = "http://192.168.56.1:3000/addmessageuser";
 
-function display_message(value_message){
-    let oldcontainer_p = document.querySelector('.p_message');
-    if(oldcontainer_p !== null){
-        oldcontainer_p.remove();
-    }
-    let newcontainer_p = document.createElement('div');
-        newcontainer_p.className = "p_message";
-    for( text of value_message){
-        let p = document.createElement('p');
-        if(User_nameLog === "Lanh"){
-            p.style.background = "red";
-            p.style.float= "left";
-            p.style.marginRight= "50%";
-            p.style.borderRadius = "15px";
-            p.style.height = "6vh";
-        }else{
-            p.style.background = "purple";
-            p.style.float= "right";
-            p.style.marginLeft = "50%";
-            p.style.borderRadius = "15px";
-            p.style.height = "6vh";
+function display_message(response) {
+
+    let value_message = response.data;
+    let contain_message = document.querySelector('.message');
+
+    //if the messange is not null and object of messages of array has amount of index................//
+
+    if (contain_message !== null && value_message.length !== 0) {
+        contain_message.remove()
+        console.log(contain_message);
+    };
+    //create div to store message and name under the message........................................//
+
+    let NewMessage = document.createElement('div');
+    NewMessage.className = "message";
+
+    for (let text of value_message) {
+        if (text.message !== "") {
+            console.log('It works')
+
+            //create div to contain paragraph of messages...........................................//
+
+            let para = document.createElement("div")
+            para.className = "pragraph";
+
+            //create p to store message that will be sent...........................................//
+
+            let p = document.createElement('p');
+            p.className = "text";
+            p.textContent = text.message;
+
+            //create Username to show the name of replier...........................................//
+
+            let Username = document.createElement("p");
+
+            if (text.userName == user_Login) { //if user logged in equal to name in the localstorage//
+                p.style.background = "blue";
+                p.style.color = "white";
+                p.style.borderRadius = "25px";
+                p.style.padding = "5px";
+                p.style.marginRight = "1%";
+                
+                //after we click on bold button and enter to be sent...............................//
+
+                if (text.bold == "bold") {
+                    p.style.fontWeight = "bold";
+                }
+                if (text.italic == "italic") {
+                    p.style.fontStyle = "italic";
+                }
+
+            } else {
+                
+                if (text.bold == "bold") {
+                    p.style.fontWeight = "bold";
+                }
+                if (text.italic == "italic") {
+                    p.style.fontStyle = "italic";
+                }
+
+                p.style.background = "gray";
+                p.style.color = "white";
+                p.style.borderRadius = "25px";
+                p.style.padding = "5px";
+                p.style.marginLeft = "1%";
+                p.style.marginBottom = "0%";
+                para.style.justifyContent = "flex-start";
+                para.style.alignItems = "flex-end";
+                //style of the username under the messages...........................................//
+
+                Username.textContent = text.userName;
+                Username.style.marginLeft = "2%";
+                Username.style.marginTop = "0%";
+                Username.style.fontSize = "13px";
+                Username.style.color = "#fff";
+            }
+
+            para.appendChild(p);
+            NewMessage.appendChild(para);
+            NewMessage.appendChild(Username);
+            containMessage.appendChild(NewMessage);
         }
-        p.className = "text";
-        p.textContent = text.message;
-        newcontainer_p.appendChild(p);
-        contain_message.appendChild(newcontainer_p);
     }
 }
-function add_mesage(event){
+
+function add_mesage(event) {
     event.preventDefault();
-    let message_data ={
-        message: textArea.value
+
+    //if we input message, the message feature will be shown in message.json.....................//
+
+    if (textArea.value !== "") {
+        let message_data = {
+            userName: user_Login,
+            message: textArea.value,
+            bold: styleBold,
+            italic: styleItalic,
+        }
+        axios.post(url, message_data).then(loadData);
     }
-    axios.post(url,message_data).then(response => {
-        display_message(response.data);
-    })
-    loadData();
     textArea.value = "";
 }
-function loadData() {
 
-    axios.get(url).then(response => {
-        display_message(response.data);
-    })
+function loadData() {
+    axios.get(url).then(display_message);
 }
 
 let textArea = document.querySelector('#text_entered');
 let btnsend = document.querySelector('#send');
-let contain_message = document.querySelector('.message');
+let containMessage = document.querySelector('.containMessage');
 
-let user_Login = localStorage.getItem("username");
-let user_h1 = document.querySelector('.username');
-let User_nameLog = "";
-for (let character = 0; character< user_Login.length; character++){
-    if (user_Login[character] === "L" || user_Login[character] === "a" || user_Login[character] === "n" || user_Login[character] === "h" || user_Login[character] === "D" || user_Login[character] === "y" || user_Login[character] === "n" || user_Login[character] === "a"){
-        User_nameLog += user_Login[character];
-    }
-}
-console.log(User_nameLog);
-user_h1.textContent = user_Login;
+let user_Login = JSON.parse(localStorage.getItem("username"));
+// let user_h1 = document.querySelector('#user');
+// user_h1.textContent = user_Login;
 
 btnsend.addEventListener('click', add_mesage)
 
-setInterval(loadData,50)
+setInterval(loadData, 1000);
 
+//italic.......................................................................................//
+
+function changeToitalic() {
+    if (textArea.value !== "") {
+        textArea.style.fontStyle = "italic";
+        styleItalic = "italic";
+    } else {
+        styleItalic = "";
+    }
+}
+
+//Bold........................................................................................//
+
+function changeTobold() {
+    if (textArea.value !== "") {
+        textArea.style.fontWeight = "bold";
+        styleBold = "bold";
+    } else {
+        styleBold = "";
+    }
+}
+
+let styleBold = "";
+let styleItalic = "";
+
+
+function isNotchange() {
+    if (textArea.value == "") {
+        textArea.style.fontStyle = "normal";
+        textArea.style.fontWeight = "normal";
+        styleBold = "";
+        styleItalic = "";
+    }
+}
+
+
+let italic = document.querySelector("#italic");
+let bold = document.querySelector("#bold");
+
+italic.addEventListener("click", changeToitalic);
+bold.addEventListener("click", changeTobold);
